d09a3dfdd7553815f148dc19b25d89587c203cb9
envomp@ttu.ee <envomp@ttu.ee>
2019-11-08 16:35:58 +0200

    java-tester docker

--- src/main/java/ee/taltech/arete/service/runner/JobRunnerServiceImpl.java
+++ src/main/java/ee/taltech/arete/service/runner/JobRunnerServiceImpl.java
@@ -2,19 +2,22 @@ package ee.taltech.arete.service.runner;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.CreateContainerResponse;
-import com.github.dockerjava.api.command.InspectContainerResponse;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.command.BuildImageResultCallback;
 import com.github.dockerjava.core.command.WaitContainerResultCallback;
 import ee.taltech.arete.domain.Submission;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.utils.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.List;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 @Service
@@ -35,20 +38,21 @@ public class JobRunnerServiceImpl implements JobRunnerService {
 //        }
 	}
 
+
+	/**
+	 * @param testJob : test job to be tested.
+	 *                testJob.getHash().substring(0, 8).lower() : image name and container name
+	 *                <p>
+	 *                input(testJob) > create image > create docker > start docker > read tester output to file
+	 */
 	public void runDocker(Submission testJob) {
 		DockerClient dockerClient = null;
 		CreateContainerResponse container = null;
 		String imageId;
-//
-//	    String hash = RandomStringUtils.random(10, true, true);
-		String command = String.format(
-				"docker build --build-arg uniid=%s --build-arg slug=%s -f Dockerfile-java-test-job -t %s . && docker run -it %s",
-				testJob.getUniid(),
-				testJob.getSlug(),
-				testJob.getHash(),
-				testJob.getHash());
-		List<String> dockerBuild = Arrays.asList(command.split(" "));
-		LOGGER.info("Docker configuration: {}", dockerBuild);
+
+		String containerName = testJob.getHash().substring(0, 8).toLowerCase();
+		String containerFile = "/output/output.json";
+		String hostFile = String.format("output/%s.json", containerName);
 
 		try {
 
@@ -62,27 +66,32 @@ public class JobRunnerServiceImpl implements JobRunnerService {
 							.withBaseDirectory(new File("./"))
 							.withDockerfile(new File("Dockerfile-java-test-job"))
 							.withPull(true)
-							.withTag(testJob.getHash())
+							.withTag(containerName)
 							.withNoCache(false)
 							.exec(new BuildImageResultCallback())
 							.awaitImageId();
 			LOGGER.info("Created image with id: {}", imageId);
 
-			container = dockerClient.createContainerCmd(testJob.getHash() + ":latest").exec();
+			container = dockerClient.createContainerCmd(containerName + ":latest").withName(containerName).exec();
 			LOGGER.info("Created container with id: {}", container.getId());
 
 			dockerClient.startContainerCmd(container.getId()).exec();
 			LOGGER.info("Started container with id: {}", container.getId());
 
-			InspectContainerResponse inspectContainerResponse = dockerClient.inspectContainerCmd(container.getId()).exec();
-			System.out.println(inspectContainerResponse);
-			System.out.println();
+			Integer statusCode = dockerClient.waitContainerCmd(container.getId())
+					.exec(new WaitContainerResultCallback())
+					.awaitStatusCode();
+			LOGGER.info("Docker finished with status code: {}", statusCode);
 
-//			dockerClient.startContainerCmd(container.getId()).exec();
-//			System.out.println(dockerClient.waitContainerCmd(container.getId()).exec(new WaitContainerResultCallback()).awaitStatusCode());
-//
-//			inspectContainerResponse = dockerClient.inspectContainerCmd(container.getId()).exec();
-//			System.out.println(inspectContainerResponse);
+			try {
+				InputStream inputStream = dockerClient.copyArchiveFromContainerCmd(containerName, containerFile).exec();
+				TarArchiveInputStream tarStream = new TarArchiveInputStream(inputStream);
+				unTar(tarStream, new File(hostFile));
+			} catch (Exception tar) {
+				tar.printStackTrace();
+			}
+			LOGGER.info("Copying output from container: {}", container.getId());
+
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -91,26 +100,42 @@ public class JobRunnerServiceImpl implements JobRunnerService {
 
 		try {
 
-//			if (dockerClient != null && container != null) {
-//
-//				LOGGER.info("Stopping container: {}", container.getId());
-//				try {
-//					dockerClient.stopContainerCmd(container.getId()).withTimeout(20).exec();
-//				} catch (Exception stop) {
-//					LOGGER.error("Container {} didnt start to begin with", container.getId());
-//				}
-//
-//				LOGGER.info("Removing container: {}", container.getId());
-//				try {
-//					dockerClient.removeContainerCmd(container.getId()).exec();
-//				} catch (Exception image) {
-//					LOGGER.error("Container {} wasn't created to begin with", testJob.getHash());
-//				}
-//			}
+			if (dockerClient != null && container != null) {
+
+				LOGGER.info("Stopping container: {}", container.getId());
+				try {
+					dockerClient.stopContainerCmd(container.getId()).withTimeout(20).exec();
+				} catch (Exception stop) {
+					LOGGER.error("Container {} has already been stopped", container.getId());
+				}
+
+				LOGGER.info("Removing container: {}", container.getId());
+				try {
+					dockerClient.removeContainerCmd(container.getId()).exec();
+				} catch (Exception image) {
+					LOGGER.error("Container {} has already been removed", testJob.getHash());
+				}
+			}
 
 		} catch (Exception finish) {
 			LOGGER.error("Container {} failed to start", container.getId());
 		}
+	}
+
+	private static void unTar(TarArchiveInputStream tis, File destFile) throws IOException {
+		TarArchiveEntry tarEntry = null;
+		while ((tarEntry = tis.getNextTarEntry()) != null) {
+			if (tarEntry.isDirectory()) {
+				if (!destFile.exists()) {
+					destFile.mkdirs();
+				}
+			} else {
+				FileOutputStream fos = new FileOutputStream(destFile);
+				IOUtils.copy(tis, fos);
+				fos.close();
+			}
+		}
+		tis.close();
 	}
 
 }
--- src/test/java/ee/taltech/arete/initializers/SubmissionInitializer.java
+++ src/test/java/ee/taltech/arete/initializers/SubmissionInitializer.java
@@ -1,6 +1,7 @@
 package ee.taltech.arete.initializers;
 
 import ee.taltech.arete.domain.Submission;
+import org.apache.commons.lang.RandomStringUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -10,31 +11,28 @@ import java.util.Random;
 
 public class SubmissionInitializer {
 	private final static String UNIID = "envomp";
-	private final static String HASH = "hash";
 	private final static String SLUG = "EX01IdCode";
 	private final static String TESTING_PLATFORM = "java";
 	private final static String RETURN_URL = "neti.ee";
 	private final static String[] EXTRA = new String[]{"style"};
-//	final static long TIMESTAMP = System.currentTimeMillis();
-//	final static Integer PRIORITY = new Random().nextInt(10) + 1;
 
 	public static Submission getFullSubmission() {
 		return Submission.builder()
 				.uniid(UNIID)
-				.hash(HASH)
+				.hash(getRandomHash())
 				.testingPlatform(TESTING_PLATFORM)
 				.returnUrl(RETURN_URL)
 				.extra(EXTRA)
 				.slug(SLUG)
 				.timestamp(System.currentTimeMillis())
-				.priority( new Random().nextInt(10) + 1)
+				.priority(new Random().nextInt(10) + 1)
 				.build();
 	}
 
 	public static Submission getControllerEndpointSubmission() {
 		return Submission.builder()
 				.uniid(UNIID)
-				.hash(HASH)
+				.hash(getRandomHash())
 				.testingPlatform(TESTING_PLATFORM)
 				.returnUrl(RETURN_URL)
 				.extra(EXTRA)
@@ -48,7 +46,7 @@ public class SubmissionInitializer {
 			jsonArray.put(extra);
 		}
 		jsonObject.put("uniid", UNIID);
-		jsonObject.put("hash", HASH);
+		jsonObject.put("hash", getRandomHash());
 		jsonObject.put("testingPlatform", TESTING_PLATFORM);
 		jsonObject.put("returnUrl", RETURN_URL);
 		jsonObject.put("extra", jsonArray);
@@ -56,10 +54,14 @@ public class SubmissionInitializer {
 		return jsonObject.toString();
 	}
 
+	private static String getRandomHash() {
+		return RandomStringUtils.random(64, true, true).toLowerCase();
+	}
+
 
 	public static void assertFullSubmission(Submission submission) {
 		assert submission.getUniid().equals(UNIID);
-		assert submission.getHash().equals(HASH);
+		assert submission.getHash().length() == 64;
 		assert submission.getReturnUrl().equals(RETURN_URL);
 		assert submission.getTestingPlatform().equals(TESTING_PLATFORM);
 		assert Arrays.equals(submission.getExtra(), EXTRA);
