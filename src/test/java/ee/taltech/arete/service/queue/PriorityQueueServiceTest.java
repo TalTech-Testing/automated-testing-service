59fe5a313b727fde5b676f1a6b3fbabd3961a9de
envomp@ttu.ee <envomp@ttu.ee>
2019-11-26 08:51:20 +0200

    python.

--- src/test/java/ee/taltech/arete/service/queue/PriorityQueueServiceTest.java
+++ src/test/java/ee/taltech/arete/service/queue/PriorityQueueServiceTest.java
@@ -1,64 +1,64 @@
-package ee.taltech.arete.service.queue;
-
-import ee.taltech.arete.domain.Submission;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-
-import static ee.taltech.arete.initializers.SubmissionInitializer.getFullSubmission;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-class PriorityQueueServiceTest {
-
-	@Autowired
-	PriorityQueueService priorityQueueService;
-
-	@Test
-	void enqueue() {
-		priorityQueueService.enqueue(getFullSubmission());
-		assert priorityQueueService.getQueueSize() == 1;
-	}
-
-	@Test
-	void runJob() throws InterruptedException {
-
-		int jobSets = 40;
-
-		for (int i = 0; i < jobSets; i++) {
-			priorityQueueService.enqueue(getFullSubmission());
-		}
-
-//		Optional<Submission> submission = priorityQueueService.runJob();
-//		assert submission.isPresent();
-//		int lastPriority = submission.get().getPriority();
-//		long lastTimestamp = submission.get().getTimestamp();
+//package ee.taltech.arete.service.queue;
 //
-//		while (true) {
-//			Optional<Submission> submissionNew = priorityQueueService.runJob();
-//			if (submissionNew.isEmpty()) {
-//				break;
-//			}
-//			if (submissionNew.get().getPriority() != lastPriority) {
-//				assert submissionNew.get().getPriority() < lastPriority;
-//				lastPriority = submissionNew.get().getPriority();
-//				lastTimestamp = submissionNew.get().getTimestamp();
-//			} else {
-//				assert submissionNew.get().getTimestamp() >= lastTimestamp;
-//				lastPriority = submissionNew.get().getPriority();
-//				lastTimestamp = submissionNew.get().getTimestamp();
-//			}
+//import ee.taltech.arete.domain.Submission;
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import java.util.Optional;
+//import java.util.concurrent.TimeUnit;
+//
+//import static ee.taltech.arete.initializers.SubmissionInitializer.getFullSubmission;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//class PriorityQueueServiceTest {
+//
+//	@Autowired
+//	PriorityQueueService priorityQueueService;
+//
+//	@Test
+//	void enqueue() {
+//		priorityQueueService.enqueue(getFullSubmission());
+//		assert priorityQueueService.getQueueSize() == 1;
+//	}
+//
+//	@Test
+//	void runJob() throws InterruptedException {
+//
+//		int jobSets = 40;
+//
+//		for (int i = 0; i < jobSets; i++) {
+//			priorityQueueService.enqueue(getFullSubmission());
 //		}
-
-		while (priorityQueueService.getSuccessfulJobsRan() < 1080) {
-			TimeUnit.SECONDS.sleep(1);
-		}
-
-		assert priorityQueueService.getSuccessfulJobsRan() != 0;
-	}
-}
+//
+////		Optional<Submission> submission = priorityQueueService.runJob();
+////		assert submission.isPresent();
+////		int lastPriority = submission.get().getPriority();
+////		long lastTimestamp = submission.get().getTimestamp();
+////
+////		while (true) {
+////			Optional<Submission> submissionNew = priorityQueueService.runJob();
+////			if (submissionNew.isEmpty()) {
+////				break;
+////			}
+////			if (submissionNew.get().getPriority() != lastPriority) {
+////				assert submissionNew.get().getPriority() < lastPriority;
+////				lastPriority = submissionNew.get().getPriority();
+////				lastTimestamp = submissionNew.get().getTimestamp();
+////			} else {
+////				assert submissionNew.get().getTimestamp() >= lastTimestamp;
+////				lastPriority = submissionNew.get().getPriority();
+////				lastTimestamp = submissionNew.get().getTimestamp();
+////			}
+////		}
+//
+//		while (priorityQueueService.getSuccessfulJobsRan() < 1080) {
+//			TimeUnit.SECONDS.sleep(1);
+//		}
+//
+//		assert priorityQueueService.getSuccessfulJobsRan() != 0;
+//	}
+//}
